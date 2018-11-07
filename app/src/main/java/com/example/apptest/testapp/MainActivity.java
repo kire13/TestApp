26af6c892cec748e8ce1de43d0f279b4f8959851
kire <44845157+kire13@users.noreply.github.com>
2018-11-07 17:01:45 -0300

    test update1

--- app/src/main/java/com/example/apptest/testapp/MainActivity.java
+++ app/src/main/java/com/example/apptest/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.apptest.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+    }
+
+    void test(){
+        Toast.makeText(this, "Hola",Toast.LENGTH_LONG);
     }
 }
